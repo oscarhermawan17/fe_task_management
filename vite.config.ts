fd1ec4cf0ff38fc156b30fb320f5ec98dbadc250
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-07-17 15:13:45 +0800

    FrontEnd - Changing server port based on env environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log('VITE_PORT:', process.env.VITE_PORT)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +13,8 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "pages-assets",
+  },
+  server: {
+    port: parseInt(process.env.VITE_PORT) || 3017,
   },
 })
